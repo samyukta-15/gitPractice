c2d98ad0bc8383d3440ded67e5d2a9b521b0cf13
Vasaikar <samyukta.vasaikar@capgemini.com>
2024-01-25 14:55:24 +0530

    push for merge conflict

--- GitJava.java
+++ GitJava.java
@@ -4,6 +4,11 @@ public class GitJava {
 	
 	private String name;
 	private int age;
+	
+	public int calculation(int age) {
+		return age+10;
+		
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
